df64dd16c4f3bef21f3a8d99a614d01c2cce1237
Fernando Gomes <fernandogomes@Fernandos-MacBook-Pro.local>
2022-03-24 21:22:35 -0400

    Base App

---
+++ JsonPlaceHolderApp/Controller/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonPlaceHolderApp
+//
+//  Created by Fernando Gomes on 24/03/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = NSLocalizedString("mainView_title", comment: "Title for Navigation Bar")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
