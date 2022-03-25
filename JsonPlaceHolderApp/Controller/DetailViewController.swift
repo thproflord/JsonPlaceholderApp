75088c5a618f4197c1eea713f5b0659bd3d844f1
Fernando Gomes <fernandogomes@Fernandos-MacBook-Pro.local>
2022-03-24 23:07:21 -0400

    Navigation and visual items

---
+++ JsonPlaceHolderApp/Controller/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  JsonPlaceHolderApp
+//
+//  Created by Fernando Gomes on 24/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = NSLocalizedString("detailView_title" , comment: "Title for Navigation Bar")
+        
+    }
+    
+}
--- JsonPlaceHolderApp/Controller/MainViewController.swift
+++ JsonPlaceHolderApp/Controller/MainViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var deleteAllButon: UIButton!
+    @IBOutlet weak var listSegmented: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = NSLocalizedString("mainView_title", comment: "Title for Navigation Bar")
-        // Do any additional setup after loading the view.
+        
+        setTranslations()
+        setViews()
+        
     }
+    
+    private func setViews() {
+        
+        listSegmented.layer.backgroundColor = UIColor.white.cgColor
+        listSegmented.layer.borderWidth = 2
+        listSegmented.layer.borderColor = UIColor.systemTeal.cgColor
+        listSegmented.setTitleTextAttributes([.foregroundColor: UIColor.systemTeal], for: .normal)
+        listSegmented.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+        
+    }
+    
+    private func setTranslations(){
+        
+        self.title = NSLocalizedString("mainView_title", comment: "Title for Navigation Bar")
+        self.deleteAllButon.setTitle(NSLocalizedString("mainView_deleteAll", comment: "Text for DeleteAll Button"), for: .normal)
+    }
+    
 
-
+    
 }
-
